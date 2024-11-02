bb61a87b95c1aece584a5da217d30a087b5849ed
BalajiGovindarajan-CH <balaji.govindarajan1979@gmail.com>
2024-11-02 16:39:58 +0000

    COmmit chanhges Updated for profile page

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Balaji Govindarajan",
+  title: "Enterprise Architect",
+  email: "balaji@ex.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "balajigovindarajan",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
